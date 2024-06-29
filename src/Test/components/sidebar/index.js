a80ed731e3d77afa4ca4270a9e1bce9497e97d18
hakr.njn <visual-basic@live.com>
2024-06-29 21:02:29 +0530

    few changes

--- src/Test/components/sidebar/index.js
+++ src/Test/components/sidebar/index.js
@@ -5,34 +5,34 @@ const sidebar = () => {
   )
 }
 
-const Buttons = ({ onToday, onSevenday, onClose }) => {
+const Buttons = ({ onToday, on3Day, onSevenday, OnMonth, on3Month, on6MOnth, on1Year, onThisMOnth, onYTD }) => {
     return (
       <div className="sideButtons">
         <button className="day" onClick={onToday}>
           Today
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={on3Day}>
           Last 3 Days
         </button>
         <button className="day" onClick={onSevenday}>
           Last 7 Days
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={OnMonth}>
           Last 30 Days
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={on3Month}>
           Last 3 Months
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={on6MOnth}>
           Last 6 Months
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={on1Year}>
           Last 1 Year
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={onThisMOnth}>
           This Month
         </button>
-        <button className="day" onClick={onSevenday}>
+        <button className="day" onClick={onYTD}>
           Year to Date 
         </button>
       </div>
